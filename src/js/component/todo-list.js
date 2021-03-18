4444e18242ee75f4b25f8d299c3127384aa308c4
Lvargasp77 <vargaspaniagua77@gmail.com>
2021-03-18 18:41:19 +0000

    Listo To-Do

---
+++ src/js/component/todo-list.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { List } from "./list";
+import { Footer } from "./footer";
+
+export function TodoList() {
+	const [list, setList] = useState([]);
+	const [task, setTask] = useState("");
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		console.log(`List: ${list}`);
+	}, [list]);
+
+	const handleChange = e => {
+		if (error) {
+			setError(false);
+		}
+
+		setTask(e.target.value);
+	};
+
+	//Delete the task
+	const deleteTask = key => {
+		const newTasks = list.filter((task, index) => index !== key);
+		setList(newTasks);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		if (task === "") {
+			setError("Se necesita una tarea");
+		} else {
+			setList([...list, task]);
+		}
+
+		setTask("");
+	};
+
+	return (
+		<div className="w-50 mx-auto shadow bg-secondary bg-gradient">
+			<h6 className="w-50 mx-auto fw-light text-dark">
+				{error ? error : null}
+			</h6>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					className="list-group-item w-100"
+					type="text"
+					placeholder="Agregar tarea"
+					name="taskName"
+					value={task}
+					onChange={handleChange}
+				/>
+			</form>
+
+			<div className="bg-white">
+				<List list={list} deleteTask={deleteTask} />
+			</div>
+			<div className="footer m-3">
+				<Footer list={list} />
+			</div>
+		</div>
+	);
+}
